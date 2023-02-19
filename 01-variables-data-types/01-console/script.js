ff2ab663fc3bc97e4c45d388910caa188aa401ea
Amir Ali anwar <amiralianwar611@gmail.com>
2023-02-19 15:53:50 +0500

    added console concepts

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,19 @@
+// log number
+
+console.log(500);
+
+// log string
+console.log('Hello there');
+
+// log multiple values
+console.log('Hello there', 500,true);
+
+// log error, warnings.
+console.error('Alert');
+console.warn('Warning');
+
+// log the object as a table
+
+console.table({name:"Amir Ali Anwar", RollNo:'2020-MSCS-530',email:"amiralianwar@gmail.com"});
+
+console.timeEnd()
--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -1,6 +1,14 @@
 // Ways to declare a variable
 // var, let, const
 
+let firstName= "Amir";
+const lastName= "Ali Anwar"
+console.log(firstName);
+console.log(lastName);
+var age= 25;
+console.log(age);
+
+
 // Naming Conventions
 // - Only letters, numbers, underscores and dollar signs
 // - Can't start with a number
